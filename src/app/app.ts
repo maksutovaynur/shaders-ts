4143b4326cc9810dc935b88f38be8e8fa6e08543
Aynur Maksutov <maksutov_aynur@mail.ru>
2022-12-08 03:33:55 +0300

    remove old mandelbrot parts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,27 +1,16 @@
 import * as PIXI from 'pixi.js';
-import createFractal from './utils';
 
 
 export default function createApp(parent: HTMLElement) {
-    const frac = createFractal(parent.clientWidth, parent.clientHeight, 0.1, 256);
+    // parent.clientWidth, parent.clientHeight;
     const app = new PIXI.Application({
         resizeTo: parent,
         view: parent as HTMLCanvasElement, 
         // autoDensity: true,
         backgroundColor: 0x30cfff
     });
-    app.stage.addChild(frac.shape);
+    // app.stage.addChild(frac.shape);
     app.ticker.maxFPS = 60;
     app.ticker.add((delta) => {
-        const minSc = 10;
-        const maxSc = 10000;
-        const minV = 1.0 / maxSc;
-        const maxV = 1.0 / minSc;
-        const period = 2;
-        const seconds = new Date().valueOf() / 1000;
-        const phase = Math.sin(Math.PI * seconds / period);
-        frac.params.scale.x = (minV + maxV) / 2 + (maxV - minV) / 2 * phase;
-        frac.params.scale.y = frac.params.scale.x;
-        frac.params.center = {x: -1.235, y: 0.1}
     });
 }
--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,47 +1,11 @@
 import * as PIXI from 'pixi.js';
-let shader_vert = require('../shaders/mand.vert');
-let shader_frag = require('../shaders/mand.frag');
-let paletteSrc = require("../assets/pal.png");
 
+function nothing(): void {
 
-function loadGeometry(w: number, h: number, worldScale = 1, colorScale = 1) {
-    const X = w / 2, Y = h / 2;
-    const wX = X * worldScale, wY = Y * worldScale;
-    const cX = X * colorScale, cY = Y * colorScale;
-    return new PIXI.Geometry()
-        .addAttribute('aVertexPosition', [-X, -Y, X, -Y, X, Y, -X, Y], 2)
-        .addAttribute('aColor', [0, 0, 0, /**/ cX, 0, 0, /**/ cX, cY, 0, /**/ 0, cY, 0], 3)
-        .addAttribute('aUvs', [-wX, -wY, wX, -wY, wX, wY, -wX, wY], 2)
-        .addIndex([0, 1, 2, 0, 2, 3]);
 }
 
-class Fractal {
-    constructor(public shape: PIXI.Mesh<PIXI.Shader>, public params: any){}
+function loadShader(): PIXI.Shader {
+    return PIXI.Shader.from(undefined, undefined, {});
 }
 
-export default function createFractal(
-    x: number, 
-    y: number, 
-    minscale = 0.01, 
-    shaderIter=128
-): Fractal {
-    const scale = 2 / Math.min(x, y);
-    const uniform = {
-        tex: PIXI.Texture.from(paletteSrc),
-        scale: {type: 'v2', value: {x: 1.0, y: 1.0}},
-        center: {type: 'v2', value: {x: 0.0, y: 0.0}},
-    }
-    const shape = new PIXI.Mesh(
-        loadGeometry(x / minscale, y / minscale, scale, scale * minscale),
-        loadShader(uniform, shaderIter)
-    );
-    shape.position.set(x / 2, y / 2);
-    shape.scale.set(minscale);
-    return new Fractal(shape, uniform);
-}
-
-function loadShader(uniform: any, iter: number = 5): PIXI.Shader {
-    const fragSrc = shader_frag.replace("{iter}", iter);
-    const vertSrc = shader_vert;
-    return PIXI.Shader.from(vertSrc, fragSrc, uniform);
-}
+export default nothing;
